99780fcab63b968dd4cf515fde072a42a3271f8f
its-me-sujan <itsmesujangit@gmail.com>
2023-06-19 22:15:48 +0545

    minor changes in display

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let buttons = document.querySelectorAll('button');
 
 let string = "";
 let arr = Array.from(buttons);
+
 arr.forEach(button => {
     button.addEventListener('click', (e) =>{
         if(e.target.innerHTML == '='){
